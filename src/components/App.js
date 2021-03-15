2fab2001a1ef8be73845d0908cd6097aef52df6d
tugaimv <30296401+tugaimv@users.noreply.github.com>
2021-03-15 22:44:05 +0200

    end project

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React from "react";
 
-import youtube from '../apis/youtube';
+import youtube from "../apis/youtube";
 
-import SearchBar from './SearchBar';
-import VideoList from './VideoList';
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-
-  state = { 
+  state = {
     videos: [],
-    selectedVideo: null
+    selectedVideo: null,
   };
 
-  onTermSubmit = async term => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term
-      }
-    })
+  componentDidMount() {
+    this.onTermSubmit("plants");
+  }
 
-    this.setState({videos: response.data.items});
-  }
-  
-  onVideoSelect = video => {
-    console.log(video);
-  }
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './VideoItem.css';
+import "./VideoItem.css";
 
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div 
-      className="video-item item"
-      onClick={() => onVideoSelect(video)}
-    >
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} 
-      alt=""/>
+    <div className="video-item item" onClick={() => onVideoSelect(video)}>
+      <img
+        alt={video.snippet.title}
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
       </div>
     </div>
   );
-}
- 
+};
+
 export default VideoItem;
