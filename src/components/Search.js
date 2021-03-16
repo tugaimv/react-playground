1499c4fd9b11f1d878b3b749b61364f7ee0c7e84
tugaimv <30296401+tugaimv@users.noreply.github.com>
2021-03-16 22:34:20 +0200

    end at warning fixing

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("plants");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+
+      setResults(data);
+    };
+
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+            target="_blank"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             className="input"
             value={term}
             onChange={(e) => setTerm(e.target.value)}
-            />
+          />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
-}
- 
+};
+
 export default Search;
